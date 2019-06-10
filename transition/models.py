754b0869ed9cb2bbeb1f4406bb9930a344aa3864
Melissa Price <melissaashprice@gmail.com>
2019-06-10 13:54:21 -0400

    set and migrated models

--- transition/models.py
+++ transition/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# STATE MODEL
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    state_flag = models.TextField()
+
+    def __str__(self):
+        self.name
+
+# ORGANIZATION MODEL
+class Organization(models.Model):
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='organizations')
+    name = models.CharField(max_length=100, default='no name')
+    industry = models.CharField(max_length=100, default='no specified industry')
+    image = models.TextField(default='no org. image')
+    description = models.TextField(default='no org. descrition')
+    external_link = models.TextField(default='no external link')
+
+    def __str__(self):
+        return self.name
--- transition_django/settings.py
+++ transition_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'transition'
+    'transition',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
